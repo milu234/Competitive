86e7ad0d629d1e8889e81b580a972308f7908329
Milu234 <milanhazra234.mh@gmail.com>
2019-03-22 20:18:04 +0530

    InsertionSortPart-1

---
+++ insertionsort.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main()
+{
+
+	int data[100],n,temp,i,j;
+	printf("Enter number of terms(should be less than 100): ");
+	scanf("%d",&n);
+	printf("Enter elements: ");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&data[i]);
+	}
+	for(i=1;i<n;i++)
+	{
+		temp = data[i];
+		j=i-1;
+		while(temp<data[j] && j>=0)
+/*To sort elements in descending order, change temp<data[j] to temp>data[j] in above line.*/
+		{
+			data[j+1] = data[j];
+			printf("%d ",data[i]);
+            printf("\n");
+			j--;
+
+
+		}
+		data[j+1]=temp;
+
+	}
+
+	for(i=0; i<n; i++)
+		printf("%d ",data[i]);
+		printf("\n");
+    return 0;
+}
