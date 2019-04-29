20a96264b18907c31c9746a3b681386efdc55817
milu234 <milanhazra234.mh@gmail.com>
2019-04-29 12:34:24 +0530

    Exception Handling

--- Exceptionhandling.java
+++ Exceptionhandling.java
@@ -11,7 +11,30 @@ Error
 1. Possible to recover from the error.
 2. Errors are unchecked type and checked type. 
 3. Happens at Runtime as well as Compile time. 
-4.  application is running
+4. Application is running
 
+Exceptions 
+
+Checked
+-- An exception is checked by the compiler at compilation-time.
+-- These exceptions cannot simply be ignored, the programmer should handle these exceptions
+
+Unchecked
+-- An exceptions that occurs during execution
+-- These are also called Run-Time Exceptions.
+-- Runtime execeptions are ingored at the time of exceptions. 
+
+Exception Syntax
+
+class Exception{
+	public static void main(String args[]){
+	try{
+		//Code that may raise exception
+	}
+	catch(Exception e){
+	//rest of the program
+	}
+	}
+}
 
 */
