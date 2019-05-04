ffffb8790b0cb3f1677233f5f048627765a03eb2
milu234 <milanhazra234.mh@gmail.com>
2019-05-04 10:32:58 +0530

    User and Computer Choice ready

---
+++ rock-paper-scissor/app.js
@@ -0,0 +1,47 @@
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+function getComputerChoice() {
+	const choices = ['r','p','s'];
+	// console.log(Math.random());
+	const randomNumber = Math.floor(Math.random()*3);
+	return choices[randomNumber];
+}
+
+// console.log(getComputerChoice());
+
+
+function game(userChoice){
+	const computerChoice = getComputerChoice();
+	console.log("user choice =>" + userChoice);
+	console.log("Computer Choice =>" + computerChoice);
+
+		// console.log("**********" + userChoice);
+}
+
+// game("c");
+
+function main() {
+
+	rock_div.addEventListener('click', function () {
+	game("r");
+	})
+
+	paper_div.addEventListener('click', function () {
+	game("p");
+	})
+
+	scissors_div.addEventListener('click', function () {
+	game("s");
+	})
+
+}
+
+main();
