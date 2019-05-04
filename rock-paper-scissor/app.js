40022a5c4209c3c5cbd61fdf63ef8db31ee9575f
milu234 <milanhazra234.mh@gmail.com>
2019-05-04 11:08:51 +0530

    User and Computer Choice ready

--- rock-paper-scissor/app.js
+++ rock-paper-scissor/app.js
@@ -1,9 +1,9 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
@@ -15,13 +15,57 @@ function getComputerChoice() {
 	return choices[randomNumber];
 }
 
+function convertToWord(letter){
+	if (letter === "r") return "Rock";
+	if (letter === "p") return "Paper";
+	return "Scissors";
+}
+
+
+
+function win(userChoice, computerChoice){
+	userScore++;
+	userScore_span.innerHTML = userScore;
+	computerScore_span.innerHTML = computerScore;
+	result_p.innerHTML = convertToWord(userChoice) + " beats " + convertToWord(computerChoice) + " . You win!!!!!";
+} 
+
+function draw(){
+	
+}
+
+function lose(){
+	
+}
+
 // console.log(getComputerChoice());
 
 
 function game(userChoice){
 	const computerChoice = getComputerChoice();
-	console.log("user choice =>" + userChoice);
-	console.log("Computer Choice =>" + computerChoice);
+	switch(userChoice + computerChoice){
+		case "rs":
+		case "pr":
+		case "sp":
+			// console.log("USER WINS");
+			win(userChoice ,computerChoice);
+			break;
+		case "rp":
+		case "ps":
+		case "sr":
+			// console.log("USER LOSES");
+			lose(userChoice + computerChoice);
+			break;
+		case "rr":
+		case "pp":
+		case "ss":
+			// console.log("Its a draw");
+			draw(userChoice + computerChoice);
+			break;
+
+	}
+	// console.log("user choice =>" + userChoice);
+	// console.log("Computer Choice =>" + computerChoice);
 
 		// console.log("**********" + userChoice);
 }
